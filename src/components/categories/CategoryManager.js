da7b580c2a9254fed136d78705b445f5f93e3f69
David Clark <david.clark.1043@gmail.com>
2022-05-17 13:25:47 -0500

    Added fetch functions for categories

--- src/components/categories/CategoryManager.js
+++ src/components/categories/CategoryManager.js
@@ -6,3 +6,15 @@ import { Settings } from "../utils/Settings"
 export const getAllCategories = () => {
   return fetchIt(`${Settings.API}/categories`)
 }
+
+export const editCategory = (newCategory) => {
+  return fetchIt(`${Settings.API}/categories/${newCategory.id}`, "PUT", newCategory)
+}
+
+export const createCategory = (newCategory) => {
+  return fetchIt(`${Settings.API}/categories`, "POST", newCategory)
+}
+
+export const deleteCategory = (id) => {
+  return fetchIt(`${Settings.API}/categories/${id}`, "DELETE")
+}
