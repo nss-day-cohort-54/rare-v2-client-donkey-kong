987b263cf039e62a6307657fb54fe7a66e6b2a8b
David Clark <david.clark.1043@gmail.com>
2022-05-20 15:35:12 -0500

    Feat: reactions on post

--- src/components/reactions/ReactionsDialog.js
+++ src/components/reactions/ReactionsDialog.js
@@ -1,7 +1,7 @@
 
 import { useHistory } from "react-router-dom"
 import { useState, useEffect } from "react"
-import { getAllReactions } from "./ReactionManager"
+import { createPostReaction, getAllReactions, getPostReactions, removePostReaction } from "./ReactionManager"
 import "./ReactionDialog.css"
 
 export const ReactionDialog = ({ postId }) => {
@@ -12,53 +12,74 @@ export const ReactionDialog = ({ postId }) => {
     const [postReactions, setPostReactions] = useState([])
     const history = useHistory()
 
+    const refreshPostReactions = () => {
+        getPostReactions(postId)
+            .then(setPostReactions)
+    }
+
     useEffect(
         () => {
             getAllReactions()
                 .then(setReactions)
-                .then(() => {
-                    return getPostReactions(postId)
-                })
-                .then(setPostReactions)
+                .then(() => refreshPostReactions())
+
         }, []
     )
 
     return <div>
         {/* dialog box to handle confirmation */}
 
-            <dialog id="reactionDialog">
-                {/* interpolate the itemType */}
-                <div>Click to add Reaction</div>
-                <div id="reactionList">
-                    {
-                        reactions.map(reaction => {
-                            return <div key={`reaction-${reaction.id}`}>
-                                <img    
-                                    onClick={()=> {
-                                        
-                                    }} 
-                                    src={reaction.imageUrl}
-                                    width="30px"
-                                    height="30px" />
-                            </div>
+        <dialog id="reactionDialog">
+            {/* interpolate the itemType */}
+            <div>Click to add Reaction</div>
+            <div id="reactionList">
+                {
+                    reactions.map(reaction => {
+                        let found_post_reactions = postReactions.find(pr => {
+                            const checkUser = pr.rareUser.id === parseInt(localStorage.getItem("userId"))
+                            const checkReaction = pr.reaction.id === reaction.id
+                            if(checkUser && checkReaction){
+                                return pr
+                            }
+                            return false
                         })
-                    }
-                </div>
+                        return <div key={`reaction-${reaction.id}`}
+                            className={found_post_reactions ? "reactionSelected" : ""}>
+                            <img
+                                onClick={() => {
+                                    if (found_post_reactions) {
+                                        removePostReaction(found_post_reactions.id)
+                                            .then(() => refreshPostReactions())
+                                    } else {
+                                        createPostReaction({
+                                            postId: postId,
+                                            reactionId: reaction.id
+                                        })
+                                            .then(() => refreshPostReactions())
+                                    }
+                                }}
+                                src={reaction.imageUrl}
+                                width="30px"
+                                height="30px" />
+                        </div>
+                    })
+                }
+            </div>
 
-                <div>
-                    {/* ok button deletes the item and refreshes the list */}
+            <div>
+                {/* ok button deletes the item and refreshes the list */}
 
-                    {/* cancel button closes the dialog box */}
-                    <button
-                        onClick={(e) => {
-                            e.preventDefault()
-                            const buttonTarget = document.querySelector(`#reactionDialog`)
-                            buttonTarget.close()
-                        }}>
-                        Cancel
-                    </button>
-                </div>
-            </dialog>
+                {/* cancel button closes the dialog box */}
+                <button
+                    onClick={(e) => {
+                        e.preventDefault()
+                        const buttonTarget = document.querySelector(`#reactionDialog`)
+                        buttonTarget.close()
+                    }}>
+                    Cancel
+                </button>
+            </div>
+        </dialog>
 
 
 
@@ -70,5 +91,25 @@ export const ReactionDialog = ({ postId }) => {
         }}>
             Add Reaction
         </button>
+        <div id="reactionList">
+            {
+                reactions.map(reaction => {
+                    let found_postReactions = postReactions.filter(pr => pr.reaction.id === reaction.id)
+                    return <>
+                    {found_postReactions?.length > 0
+                    ? <div key={`reaction-${reaction.id}`}>
+                        <div>
+                        {`${found_postReactions?.length}`}
+                        </div>
+                        <img
+                            src={reaction.imageUrl}
+                            width="30px"
+                            height="30px" />
+                    </div>
+                    : null}
+                    </>
+                })
+            }
+        </div>
     </div >
 }
