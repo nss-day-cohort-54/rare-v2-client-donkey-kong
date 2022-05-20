d52b904e34b875f90f73372f1dbd1ee96ce99d69
David Clark <david.clark.1043@gmail.com>
2022-05-19 22:11:44 -0500

    Feat: list of reactions displayed

--- src/components/tags/TagManager.js
+++ src/components/tags/TagManager.js
@@ -1,17 +1,5 @@
 // fetch all the tags
-
 import { fetchIt } from "../utils/Fetch"
 import { Settings } from "../utils/Settings"
-
-// const API = 'http://localhost:8000'
-
-// export const getAllTags = () => {
-//   return fetch(`${API}/tags`, {
-//       headers: {
-//           "Authorization": `Token ${localStorage.getItem("lu_token")}`
-//       }
-//   })
-//       .then(r => r.json())
-// }
 
 export const getAllTags = () => fetchIt(`${Settings.API}/tags`)
